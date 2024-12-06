2173f084c0ef03341f4a82dcef0742874e1c1f96
Srijagatheeswaran.R <srijagatheeswaran@gmail.com>
2024-12-06 17:09:56 +0530

    add chech auth

--- app/Http/Controllers/ProductControler.php
+++ app/Http/Controllers/ProductControler.php
@@ -12,7 +12,7 @@ class ProductControler extends Controller
         if ($request->session()->has('loggedInUser')) {
             return view('/productCreate');
         }
-        redirect('/home');
+        return redirect('/login');
     }
     function createProduct(Request $request)
     {
@@ -33,9 +33,9 @@ class ProductControler extends Controller
 
             } else {
                 $file = $request->file('image');
-                $imageName = time().'.'.$request->image->extension(); 
+                $imageName = time() . '.' . $request->image->extension();
 
-                if($request->image->move(public_path('productImg'), $imageName)){
+                if ($request->image->move(public_path('productImg'), $imageName)) {
                     $product = Products::create([
                         'product_name' => $request->input('product_name'),
                         'product_categorie' => $request->input('product_categorie'),
@@ -46,72 +46,92 @@ class ProductControler extends Controller
                     return response()->json(['status' => 200, 'message' => 'Product Created successfully']);
                 }
 
-               
+
             }
         }
-        // redirect('/home');
+        redirect('/login');
     }
-    function productEdit(int $id){
-        $product = Products::findOrFail($id);
-        return view('productEdit',compact('product'));
+    function productEdit(Request $request, int $id)
+    {
+        if ($request->session()->has('loggedInUser')) {
+            $product = Products::findOrFail($id);
+            return view('productEdit', compact('product'));
+        }
+        return redirect('/login');
+
     }
-    function productUpdate(Request $request,int $id){
-        $validator = Validator::make($request->all(), [
-            'product_name' => 'required',
-            'product_categorie' => 'required',
-            'quantity' => 'required',
-            'description' => 'required',
-            'image' => 'mimes:jpg,png,jpeg|max:2048',
-        ]);
-        if ($validator->fails()) {
-            return response()->json([
-                'status' => 400,
-                'message' => $validator->getMessageBag(),
+    function productUpdate(Request $request, int $id)
+    {
+        if ($request->session()->has('loggedInUser')) {
+            $validator = Validator::make($request->all(), [
+                'product_name' => 'required',
+                'product_categorie' => 'required',
+                'quantity' => 'required',
+                'description' => 'required',
+                'image' => 'mimes:jpg,png,jpeg|max:2048',
             ]);
-
-        }
-        else{
-            if($request->hasFile('image')){
-                $file = $request->file('image');
-                $imageName = time().'.'.$request->image->extension(); 
-
-                if($request->image->move(public_path('productImg'), $imageName)){
-                    products::findOrFail($id)->update([
-                        'product_name' => $request->input('product_name'),
-                        'product_categorie' => $request->input('product_categorie'),
-                        'quantity' => $request->input('quantity'),
-                        'description' => $request->input('description'),
-                        'image' => $imageName,
-                    ]);
-                    return response()->json(['status' => 200, 'message' => 'Update SuccessFully']);
-                }
-            }else{
-                products::findOrFail($id)->update(
-                    [
-                        'product_name' => $request->input('product_name'),
-                        'product_categorie' => $request->input('product_categorie'),
-                        'quantity' => $request->input('quantity'),
-                        'description' => $request->input('description'),
-                    ]
-                );
+            if ($validator->fails()) {
                 return response()->json([
-                    'status'=> 200,
-                    'message'=> 'Update SuccessFully'
+                    'status' => 400,
+                    'message' => $validator->getMessageBag(),
                 ]);
+
+            } else {
+                if ($request->hasFile('image')) {
+                    $file = $request->file('image');
+                    $imageName = time() . '.' . $request->image->extension();
+
+                    if ($request->image->move(public_path('productImg'), $imageName)) {
+                        products::findOrFail($id)->update([
+                            'product_name' => $request->input('product_name'),
+                            'product_categorie' => $request->input('product_categorie'),
+                            'quantity' => $request->input('quantity'),
+                            'description' => $request->input('description'),
+                            'image' => $imageName,
+                        ]);
+                        return response()->json(['status' => 200, 'message' => 'Update SuccessFully']);
+                    }
+                } else {
+                    products::findOrFail($id)->update(
+                        [
+                            'product_name' => $request->input('product_name'),
+                            'product_categorie' => $request->input('product_categorie'),
+                            'quantity' => $request->input('quantity'),
+                            'description' => $request->input('description'),
+                        ]
+                    );
+                    return response()->json([
+                        'status' => 200,
+                        'message' => 'Update SuccessFully'
+                    ]);
+                }
             }
-                
-            
-            
+            return redirect('/login');
+
+
+
         }
-        
+
     }
-    function productDelete(int $id){
-        
-        $prodect = Products::findOrFail($id);
-        $prodect->delete();
-        return redirect()->back()->with('success','Product delete Successfully');
+    function productDelete(Request $request,int $id)
+    {
+
+        if ($request->session()->has('loggedInUser')) {
+            $prodect = Products::findOrFail($id);
+            $prodect->delete();
+            return redirect()->back()->with('success', 'Product delete Successfully');
+        }
+        return redirect('/login');
+
     }
-    function showOrders(){
-        return view('orders');
+    function showOrders(Request $request)
+    {
+        if ($request->session()->has('loggedInUser')) {
+            return view('orders');
+        }
+        return redirect('/login');
+
+
+
     }
 }
